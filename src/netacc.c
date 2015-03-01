edee93d967da9f1f012cb4d059d535cef5004366
Chris Cox <crcox@wisc.edu>
2015-02-28 18:53:22 -0600

    Working version of netacc. Only accuracy method implemented is: is the activation closer to the intended target than any other possible target?

--- src/netacc.c
+++ src/netacc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <lens.h>
 #include "util.h"
@@ -9,7 +10,7 @@
 #include <argp.h>
 
 const char *argp_program_version =
-"layeract 0.1";
+"netacc 0.1";
 
 const char *argp_program_bug_address =
 "<cox.crc@gmail.com>";
@@ -97,13 +98,12 @@ static struct argp argp = {options, parse_opt, args_doc, doc};
 
 int main(int argc, char *argv[]) {
   struct arguments arguments;
-  int nexamples;
-  int nunits;
-  int nticks;
+  int nevents,nticks;
   int gn;
+  int acc;
   int tickstart=0;
-  double unitact;
-  double unittarg;
+  float  unitact, unittarg;
+  float dist, selfdist, otherdist, minotherdist;
 
   /* Set argument defaults */
   arguments.index = 0;
@@ -125,18 +125,20 @@ int main(int argc, char *argv[]) {
 
   /* Source the network file */
   lens("source %s", netfilename);
+
   /* Load the weights */
   lens("loadWeights %s", weightfilename);
   /* Load the examples */
   lens("loadExamples %s -set testset -exmode ORDERED", examplefilename);
   lens("useTestingSet testset");
   /* Number of examples */
-  nexamples=Root->set[0]->numExamples;
+  const int nexamples=Root->set[0]->numExamples;
+
   /* Figure out the group number of associated with layer name */
   lens("path -group %s", layername);
   sscanf(Tcl_GetStringResult(Interp), "group(%d)", &gn);
   /* Number of units in layer */
-  nunits = Net->group[8]->numUnits;
+  const int nunits = Net->group[8]->numUnits;
 
   if (arguments.verbose) {
     fprintf(stderr,"Network:        %s\n", netfilename);
@@ -145,28 +147,60 @@ int main(int argc, char *argv[]) {
     fprintf(stderr,"Weights:        %s\n",weightfilename);
     fprintf(stderr,"-t:        %d\n",arguments.tick);
   }
-/*
-  for (int iex=0; iex<nexamples; iex++) {
-    if(arguments.index==1) {fprintf(stdout,"%d ",iex);}
+
+  /* Iterator variables */
+  int i;
+  int iex;
+  int jex;
+  int itick;
+  for (iex=0; iex<nexamples; iex++) {
+    // We will assume for now that the last event is where targets are //
+    nevents = Root->set[0]->example[iex]->numEvents;
     lens("doExample %d -set testset -test", iex);
     nticks = Net->ticksOnExample;
+    // Print example index if asked for
+    if(arguments.index==1) {fprintf(stdout,"%d ",iex);}
+    // In continuous networks (where nticks>1) tick[0] is used as setup, and is
+    // not intersting.
     if (nticks>1){tickstart=1;}
+    // If a specific tick is specified "loop" over just that tick.
     if (arguments.tick>0) {
       tickstart=arguments.tick;
       nticks = tickstart+1;
     }
-    if(arguments.verbose) {fprintf(stderr,"Ticks: %d\n",nticks);}
 
-    for (int itick=tickstart; itick<nticks; itick++) {
-      if(arguments.index==1) {fprintf(stdout,"%d ",itick);}
-      for (int i=0;i<nunits; i++) {
-        unittarg = Net->group[gn]->unit[i].targetHistory[itick];
-        unitact = Net->group[gn]->unit[i].outputHistory[itick];
-        fprintf(stdout,"%.3f ",unitact);
+    acc = 0;
+    for (itick=tickstart; itick<nticks; itick++) {
+      // Skip over ticks belonging to events without targets
+      if (Net->eventHistory[itick]!=(nevents-1)) {continue;}
+      // Print tick index if asked for
+      //if(arguments.index==1) {fprintf(stdout,"%d ",itick);}
+
+      // What we want to know is: are the activations for this example closer
+      // to the intended target than the target activation associated with any
+      // other esample?
+      minotherdist = 9999;
+      selfdist = 0;
+      for (jex=0; jex<nexamples; jex++) {
+        otherdist = 0;
+        for (i=0;i<nunits; i++) {
+          unittarg = Root->set[0]->example[jex]->event[nevents-1].target->val[i];
+          unitact = Net->group[gn]->unit[i].outputHistory[itick];
+          dist = pow(unitact-unittarg, 2);
+          if (iex==jex) {
+            selfdist = selfdist + dist;
+          } else {
+            otherdist = otherdist + dist;
+          }
+        }
+        if (iex!=jex && otherdist < minotherdist) {
+          minotherdist = otherdist;
+       }
       }
-      fprintf(stdout,"\n");
+      if (selfdist < minotherdist) {acc = 1;}
+      fprintf(stdout,"%d ",acc);
     }
+    fprintf(stdout,"\n");
   }
-  */
   return 0;
 }
